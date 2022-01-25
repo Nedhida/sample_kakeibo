4bee23399bd76d835806b33f157693197ad13a4e
Nedhida <pinedoutshadeyouth@gmail.com>
2022-01-25 02:25:23 +0000

    [Add]Fixedcosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_012029) do
+ActiveRecord::Schema.define(version: 2022_01_25_020511) do
+
+  create_table "fixedcosts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "incomes", force: :cascade do |t|
     t.string "name", null: false
