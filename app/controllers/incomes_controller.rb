f89c00553f501b9bdddde61624d69e7dae78c44b
Nedhida <pinedoutshadeyouth@gmail.com>
2022-01-25 02:04:24 +0000

    [Fin]SimpleIncomes

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -6,21 +6,40 @@ class IncomesController < ApplicationController
   end
 
   def show
+    @income = Income.find(params[:id])
   end
 
   def new
+    @income = Income.new()
   end
 
   def edit
+    @income = Income.find(params[:id])
   end
 
   def create
+    @income = Income.new(params[:income])
+    if @income.save
+      redirect_to @income, notice: "収入科目を登録しました。"
+    else
+      render "new"
+    end
   end
 
   def update
+    @income = Income.find(params[:id])
+    @income.assign_attributes(params[:income])
+    if @income.save
+      redirect_to @income, notice: "収入科目を更新しました。"
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @income = Income.find(params[:id])
+    @income.destroy
+    redirect_to :incomes, notice: "科目を削除しました。"
   end
 
 end
